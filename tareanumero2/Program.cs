d2db0647ca5ff2dc7342e682801ef5f2dcd085f4
arayadani <arayadani33@gmail.com>
2022-10-03 16:28:33 -0600

    tarea2

---
+++ tareanumero2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tareanumero2
+{
+    internal class Program
+    {
+        private static object item;
+
+        static void Main(string[] args)
+        {
+            string[] empleado = { "daniela", "ale", "migue" };
+            int[] salario = { 2000, 3400, 3000 };
+            int aguinaldo = 0;
+            foreach (string item in empleado)
+            {
+                Console.WriteLine(item);
+            }
+            foreach (int item in salario)
+            {
+                Console.WriteLine("Su salario actual: "+item);
+            }
+            for (int i = 0; i < salario.Length; i++)
+            {
+                aguinaldo= salario[i]*12;
+                Console.WriteLine("su aguinaldo " + aguinaldo);
+
+            }
+
+            
+
+
+            }
+    }
+}
